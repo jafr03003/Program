385882109bb207495e49f0d3e0511e91b47043ee
jafr03003 <70271892+jafr03003@users.noreply.github.com>
2020-11-23 16:00:41 +0100

    Add files via upload

---
+++ Gissa tal konsol2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gissa_tal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int antal = 0;
+            Random slump = new Random();
+            int random = slump.Next(0, 101);
+            Console.WriteLine("Gissa ett tal från 1-100: ");
+            int tal = Convert.ToInt32(Console.ReadLine());
+            antal++;
+            {
+                while (tal != random)
+                {
+                    if (tal < random)
+                    {
+                        Console.WriteLine("Det är för lågt");
+                        Console.WriteLine("Gissa igen");
+                        antal++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("De för högt");
+                        Console.WriteLine("gissa igen");
+                        antal++;
+                    }
+                    tal = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine("Din gissning var rätt");
+                Console.WriteLine("Dina försök: " + antal);
+            }
+        }
+    }
+}
---
+++ gissa forms2/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gissa_forms2
+{
+    public partial class Form1 : Form
+    {
+        Random slump;
+        int antal = 0;
+        int tal = 0;
+        int random = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            slump = new Random();
+            random = slump.Next(0, 101);
+        }
+
+        private void Aaaa_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            
+            tal = int.Parse(textBox2.Text);
+            antal++;
+            {
+                if (tal < random)
+                {
+                    label1.Text = ("Det är för lågt");
+                    label2.Text = ("Gissa igen");
+                    
+
+                }
+                else if (tal > random)
+                {
+                    label1.Text = ("Det för högt");
+                    label2.Text = ("Gissa igen");
+                    
+                }
+                else
+                    label1.Text = ("Rätt, Dina försök: " + antal);
+                    label2.Text = ("");
+
+
+
+            }
+        }
+    }
+}
+    
+
+
+
+
+
+
